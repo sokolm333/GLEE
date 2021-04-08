e8858d7995b3910cf008ba5aa3dd698dbccdad7d
sokolm333 <sokolm333@gmail.com>
2021-04-08 21:47:47 +0300

    added block with lamps, section "New Design", block with video, section "Our Insights & Articles", block with partners slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,20 @@ $(function () {
 
 	$('.product-slider__top').slick({
 		dots: true,
-		// autoplay: true,
+		autoplay: true,
 		autoplaySpeed: 5000,
 		arrows: false,
+		slidesToShow: 1,
 	});
 
-
+	$('.partners__items').slick({
+		arrows: false,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		variableWidth: true,
+		centerPadding: "0",
+	});
 
 	var containerEl1 = document.querySelector('[data-ref="container-1"]');
 	var containerEl2 = document.querySelector('[data-ref="container-2"]');
