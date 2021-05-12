21de7fbeab4cd0f1389ae978bb83213693446f3d
sokolm333 <sokolm333@gmail.com>
2021-05-12 17:41:39 +0300

    add rateYo

--- gulpfile.js
+++ gulpfile.js
@@ -116,6 +116,7 @@ function scripts() {
 		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
 		'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
 		'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+		'node_modules/rateyo/src/jquery.rateyo.js',
 		'app/js/main.js'
 	])
 		.pipe(concat('main.min.js'))
